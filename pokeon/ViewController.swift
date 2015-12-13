5779e03866295744e14a1fd187492384c1b779a3
SherifKamalSalem <sherif@sherifs-mac.local>
2015-12-13 10:22:49 -0800

    Initial Commit

---
+++ pokeon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokeon
+//
+//  Created by Sherif Kamal on ١٣‏/١٢‏/٢٠١٥.
+//  Copyright © ٢٠١٥ Sherif Kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
